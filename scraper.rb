59e7f5233cb6163b6e66201b9a04adf121a36c18
Chin Wang <chinwang@ChindeMacBook-Air.local>
2014-10-09 22:55:12 +0800

    Add scrape author name.

--- scraper.rb
+++ scraper.rb
@@ -1,3 +1,8 @@
 require 'open-uri'
+require 'nokogiri'
 
 web_data = open("https://kindle.amazon.com/")
+doc = Nokogiri.HTML(web_data)
+
+people = doc.xpath("//div[@class='name']")
+people.each {|followed| puts followed.text}
