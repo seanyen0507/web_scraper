78425ceb1373afb0f927654da6764e8aee65dde9
Sean Hsieh <seanyen0507@gmail.com>
2014-10-12 23:51:10 +0800

    Final version

---
+++ application.rb
@@ -0,0 +1,3 @@
+require './scraper.rb'
+sam = Scraper.new
+sam
--- scraper.rb
+++ scraper.rb
@@ -1,6 +1,7 @@
 require 'open-uri'
 require 'nokogiri'
 
+# this is a class
 class Scraper
   web_data = open('http://scores.espn.go.com/nba/scoreboard')
   doc = Nokogiri.HTML(web_data)
@@ -12,16 +13,24 @@ class Scraper
   team = doc.xpath("//div[@class='team-capsule']//a")
   team.each { |teams| y << teams.text }
   player = doc.xpath("//div[@style='display: block']//table[@class='game-stat-overview']//tbody//tr//td")
-  player.each { |x| team_1 << x.text }
+  player.each { |p| team_1 << p.text }
 
   for i in 0..(y.size) / 2 - 1
     z << y[i * 2] + ' v.s ' + y[i * 2 + 1]
   end
 
   for i in 0..(team_1.size) / 5 - 1
-    w << team_1[i * 5] + "\t" + "#{team_1[i * 5 + 1]} #{team_1[i * 5 + 2]}" + ' v.s ' + "#{team_1[i * 5 + 3]} #{team_1[i * 5 + 4]}"
+    w << team_1[i * 5] + "\t" + "#{team_1[i * 5 + 1]} #{team_1[i * 5 + 2]}" + \
+    ' v.s '  + "#{team_1[i * 5 + 3]} #{team_1[i * 5 + 4]}"
   end
+
   schedule_table_upcoming = Hash[x.zip(z)]
-  schedule_table_upcoming.each { |key, value| puts "#{key}\t#{value}" }
-  w.each { |x| puts "#{x}" }
+  schedule_table_upcoming.each do |key, value|
+    puts "#{key}\t#{value}"
+    if w.size >= 5
+      5.times { puts w.shift }
+    else
+      w.size.times { puts w.shift }
+    end
+  end
 end
