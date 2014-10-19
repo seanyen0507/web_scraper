6c8bcd9f9ec2455e20bc1275704937e8275a96c0
hsuchinwang <sam30278@gmail.com>
2014-10-19 13:10:18 +0800

    can choose the player by user now

--- scraper.rb
+++ scraper.rb
@@ -5,8 +5,15 @@ require 'nokogiri'
 class Scraper
   web_data = open('http://scores.espn.go.com/nba/scoreboard')
   doc = Nokogiri.HTML(web_data)
-  x, y, z, w = [], [], [], []
+  web_data_player = 'http://origin.nba.com/playerfile/'
+  x, y, z, w ,f = [], [], [], [], []
   team_1 = []
+  data = ["PTS", "REB", "AST", "PIE"]
+
+  puts "Which player's profile you want to see?"
+  name = gets.chomp
+  web_data_player = web_data_player + name
+  doc1 = Nokogiri.HTML(open(web_data_player))
 
   time = doc.xpath("//div[@class='game-status']//p")
   time.each { |times| x << times.text }
@@ -15,6 +22,9 @@ class Scraper
   player = doc.xpath("//div[@style='display: block']\
   //table[@class='game-stat-overview']//tbody//tr//td")
   player.each { |p| team_1 << p.text }
+
+  profile = doc1.xpath("//div[@class='sponsor-branding']//tr[@class='stats text-shadow']//td")
+  profile.each { |profile| f << profile.text }
 
   for i in 0..(y.size) / 2 - 1
     z << y[i * 2] + ' v.s ' + y[i * 2 + 1]
@@ -25,6 +35,8 @@ class Scraper
     ' v.s '  + "#{team_1[i * 5 + 3]} #{team_1[i * 5 + 4]}"
   end
 
+  player_profile = Hash[data.zip(f)]
+  puts player_profile
   schedule_table_upcoming = Hash[x.zip(z)]
   schedule_table_upcoming.each do |key, value|
     puts "#{key}\t#{value}"
