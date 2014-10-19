1a6f1ff3c1bd07602b120da29a92426115c1ac52
Sean Hsieh <seanyen0507@gmail.com>
2014-10-19 23:11:16 +0800

    start a test file

--- scraper.rb
+++ scraper.rb
@@ -14,44 +14,44 @@ class Scraper
   control = gets.chomp
   control = control.to_i
   if control == 1
-  puts "Which player's profile do you want to search?"
-  name = gets.chomp
-  name.downcase!
-  web_data_player = web_data_player + name
-  doc1 = Nokogiri.HTML(open(web_data_player))
-  profile = doc1.xpath("//div[@class='sponsor-branding']//tr[@class='stats text-shadow']//td")
-  profile.each { |profile| f << profile.text }
-  player_profile = Hash[data.zip(f)]
-  puts player_profile
+    puts "Which player's profile do you want to search?"
+    name = gets.chomp
+    name.downcase!
+    web_data_player = web_data_player + name
+    doc1 = Nokogiri.HTML(open(web_data_player))
+    profile = doc1.xpath("//div[@class='sponsor-branding']//tr[@class='stats text-shadow']//td")
+    profile.each { |profile| f << profile.text }
+    player_profile = Hash[data.zip(f)]
+    puts player_profile
 
-elsif control == 2
-  time = doc.xpath("//div[@class='game-status']//p")
-  time.each { |times| x << times.text }
-  team = doc.xpath("//div[@class='team-capsule']//a")
-  team.each { |teams| y << teams.text }
-  player = doc.xpath("//div[@style='display: block']\
-  //table[@class='game-stat-overview']//tbody//tr//td")
-  player.each { |p| team_1 << p.text }
+  elsif control == 2
+    time = doc.xpath("//div[@class='game-status']//p")
+    time.each { |times| x << times.text }
+    team = doc.xpath("//div[@class='team-capsule']//a")
+    team.each { |teams| y << teams.text }
+    player = doc.xpath("//div[@style='display: block']\
+    //table[@class='game-stat-overview']//tbody//tr//td")
+    player.each { |p| team_1 << p.text }
 
-  for i in 0..(y.size) / 2 - 1
-    z << y[i * 2] + ' v.s ' + y[i * 2 + 1]
-  end
-
-  for i in 0..(team_1.size) / 5 - 1
-    w << team_1[i * 5] + "\t" + "#{team_1[i * 5 + 1]} #{team_1[i * 5 + 2]}" + \
-    ' v.s '  + "#{team_1[i * 5 + 3]} #{team_1[i * 5 + 4]}"
-  end
-
-  schedule_table_upcoming = Hash[x.zip(z)]
-  schedule_table_upcoming.each do |key, value|
-    puts "#{key}\t#{value}"
-    if key.include? "PM" || "AM"
-      5.times { puts w.shift }
-    else
-      3.times { puts w.shift }
+    for i in 0..(y.size) / 2 - 1
+      z << y[i * 2] + ' v.s ' + y[i * 2 + 1]
     end
+
+    for i in 0..(team_1.size) / 5 - 1
+      w << team_1[i * 5] + "\t" + "#{team_1[i * 5 + 1]} #{team_1[i * 5 + 2]}" + \
+      ' v.s '  + "#{team_1[i * 5 + 3]} #{team_1[i * 5 + 4]}"
+    end
+
+    schedule_table_upcoming = Hash[x.zip(z)]
+    schedule_table_upcoming.each do |key, value|
+      puts "#{key}\t#{value}"
+      if key.include? "PM" || "AM"
+        5.times { puts w.shift }
+      else
+        3.times { puts w.shift }
+      end
+    end
+  else
+    puts "Please enter number 1 or 2"
   end
-else
-  puts "Please enter number 1 or 2"
-end
 end
---
+++ test.rb
@@ -0,0 +1,3 @@
+require 'minitest/autorun'
+require 'minitest/rg'
+require './scraper.rb'
