29f646cd0c30f453ba9d0e9b1f981fe2397331f8
hsuchinwang <sam30278@gmail.com>
2014-10-19 23:42:53 +0800

    modified the function and application.rb

--- application.rb
+++ application.rb
@@ -1,3 +1,14 @@
-require './scraper.rb'
+require './scraper'
 sam = Scraper.new
-sam
+puts "Enter 1 to see starting lineup today, enter 2 to see any player's profile you want."
+control = gets.chomp
+control = control.to_i
+
+if control == 1
+  puts "Which player's profile you want to see?"
+  name = gets.chomp
+  sam.profile(name)
+elsif control == 2
+
+  sam.game()
+end
--- scraper.rb
+++ scraper.rb
@@ -3,47 +3,56 @@ require 'nokogiri'
 
 # this is a class
 class Scraper
-  web_data = open('http://scores.espn.go.com/nba/scoreboard')
-  doc = Nokogiri.HTML(web_data)
-  web_data_player = 'http://origin.nba.com/playerfile/'
-  x, y, z, w ,f = [], [], [], [], []
-  team_1 = []
-  data = ["PTS", "REB", "AST", "PIE"]
 
-  puts "Which player's profile you want to see?"
-  name = gets.chomp
-  web_data_player = web_data_player + name
-  doc1 = Nokogiri.HTML(open(web_data_player))
+  def game
+    x, y, z, w, team_1  = [], [], [], [], []
+    
+    doc = Nokogiri.HTML( open('http://scores.espn.go.com/nba/scoreboard') )
 
-  time = doc.xpath("//div[@class='game-status']//p")
-  time.each { |times| x << times.text }
-  team = doc.xpath("//div[@class='team-capsule']//a")
-  team.each { |teams| y << teams.text }
-  player = doc.xpath("//div[@style='display: block']\
-  //table[@class='game-stat-overview']//tbody//tr//td")
-  player.each { |p| team_1 << p.text }
+    time = doc.xpath("//div[@class='game-status']//p")
+    time.each { |times| x << times.text }
+    team = doc.xpath("//div[@class='team-capsule']//a")
+    team.each { |teams| y << teams.text }
+    player = doc.xpath("//div[@style='display: block']\
+    //table[@class='game-stat-overview']//tbody//tr//td")
+    player.each { |p| team_1 << p.text }
 
-  profile = doc1.xpath("//div[@class='sponsor-branding']//tr[@class='stats text-shadow']//td")
-  profile.each { |profile| f << profile.text }
-
-  for i in 0..(y.size) / 2 - 1
-    z << y[i * 2] + ' v.s ' + y[i * 2 + 1]
-  end
-
-  for i in 0..(team_1.size) / 5 - 1
-    w << team_1[i * 5] + "\t" + "#{team_1[i * 5 + 1]} #{team_1[i * 5 + 2]}" + \
-    ' v.s '  + "#{team_1[i * 5 + 3]} #{team_1[i * 5 + 4]}"
-  end
-
-  player_profile = Hash[data.zip(f)]
-  puts player_profile
-  schedule_table_upcoming = Hash[x.zip(z)]
-  schedule_table_upcoming.each do |key, value|
-    puts "#{key}\t#{value}"
-    if key.include? "PM" || "AM"
-      5.times { puts w.shift }
-    else
-      3.times { puts w.shift }
+    for i in 0..(y.size) / 2 - 1
+      z << y[i * 2] + ' v.s ' + y[i * 2 + 1]
     end
+
+    for i in 0..(team_1.size) / 5 - 1
+      w << team_1[i * 5] + "\t" + "#{team_1[i * 5 + 1]} #{team_1[i * 5 + 2]}" + \
+      ' v.s '  + "#{team_1[i * 5 + 3]} #{team_1[i * 5 + 4]}"
+    end
+
+    schedule_table_upcoming = Hash[x.zip(z)]
+    schedule_table_upcoming.each do |key, value|
+      puts "#{key}\t#{value}"
+      if key.include? "PM" || "AM"
+        5.times { puts w.shift }
+      else
+        3.times { puts w.shift }
+      end
+    end
+
   end
+
+  def profile(name)
+    f = []
+    data = ["PTS", "REB", "AST", "PIE"]
+    web_data_player = 'http://origin.nba.com/playerfile/'
+    web_data_player = web_data_player + name
+    doc1 = Nokogiri.HTML(open(web_data_player))
+    profile = doc1.xpath("//div[@class='sponsor-branding']\
+    //tr[@class='stats text-shadow']//td")
+    profile.each { |profile| f << profile.text }
+    player_profile = Hash[data.zip(f)]
+    player_profile.each {
+      |key, value| puts "#{key} : #{value}"
+    }
+
+  end
+
+
 end
