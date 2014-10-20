a9722e4b8fca999d1d21448d5c990ced1754b8aa
PoWeiHuang <benny59438@gmail.com>
2014-10-20 12:14:29 +0800

    Revise a stubid bug.

--- application.rb
+++ application.rb
@@ -6,7 +6,7 @@ puts "Enter 1 to see starting lineup today, enter 2 to see any player's profile 
 control = gets.chomp
 control = control.to_i
 
-if control == 1
+if control == 2
   puts "Which player's profile you want to see?"
   name = gets.chomp
   name.downcase!
@@ -15,7 +15,7 @@ if control == 1
   sean.each {
     |key, value| puts "#{key} : #{value}"
   }
-elsif control == 2
+elsif control == 1
   po = sam.game[0]
   w = sam.game[1]
   po.each do |key, value|
