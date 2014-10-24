5058c45004fceca3ec5707a3a34804edac9a97b9
hsuchinwang <sam30278@gmail.com>
2014-10-24 15:36:20 +0800

    Solve the problem due to the modify of website.

--- lib/NBA_info.rb
+++ lib/NBA_info.rb
@@ -2,33 +2,46 @@ require 'open-uri'
 require 'nokogiri'
 # this is a class
 class Scraper
-
-
   def game
-    x, y, z, w, team_1  = [], [], [], [], []
+    x, y, z, w, team_1, start_line_up, s = [], [], [], [], [], [], []
+    count = 1
 
     doc = Nokogiri.HTML(open('http://scores.espn.go.com/nba/scoreboard'))
 
     time = doc.xpath("//div[@class='game-status']//p")
-    time.each { |times| x << times.text }
-    team = doc.xpath("//div[@class='team-capsule']//a")
+    time.each do |times|
+      if times.text == 'Final'
+        x << times.text + ' ' + count.to_s
+        count += 1
+      else
+        x << times.text
+      end
+    end
+    team = doc.xpath("//div[@class='team-capsule']//span")
     team.each { |teams| y << teams.text }
+    score = doc.xpath("//div[@class='mod-content']//ul[@class='score']\
+    //li[@class='finalScore']")
+    score.each { |scores| team_1 << scores.text }
     player = doc.xpath("//div[@style='display: block']\
     //table[@class='game-stat-overview']//tbody//tr//td")
-    player.each { |p| team_1 << p.text }
+    player.each { |p| start_line_up << p.text }
 
     for i in 0..(y.size) / 2 - 1
       z << y[i * 2] + ' v.s ' + y[i * 2 + 1]
     end
 
-    for i in 0..(team_1.size) / 5 - 1
-      w << team_1[i * 5] + "\t" + "#{team_1[i * 5 + 1]} #{team_1[i * 5 + 2]}" + \
-      ' v.s '  + "#{team_1[i * 5 + 3]} #{team_1[i * 5 + 4]}"
+    for i in 0..(team_1.size) / 2 - 1
+      w << team_1[i * 2] + ' v.s ' + team_1[i * 2 + 1]
+    end
+
+    for i in 0..(start_line_up.size) / 5 - 1
+      s << start_line_up[i * 5] + "\t" + "#{start_line_up[i * 5 + 1]} "\
+      "#{start_line_up[i * 5 + 2]}" + ' v.s '  + "#{start_line_up[i * 5 + 3]} "\
+      "#{start_line_up[i * 5 + 4]}"
     end
 
     schedule_table_upcoming = Hash[x.zip(z)]
-    [schedule_table_upcoming, w]
-
+    [schedule_table_upcoming, w, s]
   end
 
   def profile(name)
@@ -43,5 +56,4 @@ class Scraper
     player_profile = Hash[data.zip(f)]
     [player_profile, f]
   end
-
 end
